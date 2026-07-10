a29267806495578e06a909f52ffd6021c0f815d4
Zegatlord <alex.krzak@icloud.com>
2026-07-10 12:53:34 +0200

    animacja chodzenia

--- scripts/player 1.gd
+++ scripts/player 1.gd
@@ -47,6 +47,11 @@ func _physics_process(delta: float) -> void:
 		else:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
 		#animacje
+		if is_on_floor() and direction != 0 and SPEED < 1200:
+			animation_sprite.play("walk")
+		elif is_on_floor():
+			animation_sprite.play("Idle")
+		
 		if direction < 0:
 			animation_sprite.flip_h = true
 		else:
