a1c04b797898c038d2f40036574409b5bd9783a2
Zegatlord <alex.krzak@icloud.com>
2026-07-13 16:58:21 +0200

    dodanie przeciwników

--- scripts/global_var.gd
+++ scripts/global_var.gd
@@ -2,3 +2,4 @@ extends Node
 
 var lifes = 1
 var no_move = false
+var player_force = 0
--- scripts/player 1.gd
+++ scripts/player 1.gd
@@ -5,7 +5,8 @@ const SPEED = 500.0
 const JUMP_VELOCITY = -700.0
 const sprint_speed = 1000.0
 var extra_jump_count = 1
-@onready var animation_sprite = $AnimatedSprite2D
+@onready var animation_sprite = $AnimationPlayer
+@onready var anim = $AnimatedSprite2D
 @onready var dash_timer: Timer = $dash_timer
 @onready var next_dash_timer: Timer = $next_dash_timer
 
@@ -54,6 +55,8 @@ func _physics_process(delta: float) -> void:
 	if Global.no_move == false:
 		var direction := Input.get_axis("lewo_gracz_1", "prawo_gracz_1")
 		if direction:
+			Global.player_force = velocity.x
+			print(velocity.x)
 			#sprintowanie + dash
 			if Input.is_action_pressed("sprint") and dashing:
 				velocity.x = direction * (sprint_speed + DASH_SPEED)
@@ -67,7 +70,7 @@ func _physics_process(delta: float) -> void:
 				velocity.x = direction * SPEED
 		else:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
-		#animacje
+		#animacjew
 		if dashing == true and direction  != 0:
 			animation_sprite.play("dashing")
 		elif is_on_floor() and direction != 0 and Input.is_action_pressed("sprint"):
@@ -82,9 +85,12 @@ func _physics_process(delta: float) -> void:
 			animation_sprite.play("Idle")
 		
 		if direction < 0:
-			animation_sprite.flip_h = true
+			anim.flip_h = true
+			$HitBox/CollisionShape2D.position.x = -99
 		elif direction > 0:
-			animation_sprite.flip_h = false
+			anim.flip_h = false
+			$HitBox/CollisionShape2D.position.x = 99
+		
 	else:
 		velocity.x = 0
 	move_and_slide()
