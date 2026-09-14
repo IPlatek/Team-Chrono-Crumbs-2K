6ca069110409a67e86362fac996b0c5396653f4b
Zegatlord <alex.krzak@icloud.com>
2026-09-14 21:16:24 +0200

    ulepszenie levela bossa

--- scripts/boss_attack.gd
+++ scripts/boss_attack.gd
@@ -7,21 +7,29 @@ extends Area2D
 var active = false
 
 func _ready() -> void:
-	position.y = randf_range(260,1000)
-	boddy.color = Color(0.797, 0.0 , 0.205 ,0.5)
+	position.y = randf_range(260, 1000)
+	boddy.color = Color(0.797, 0.0, 0.205, 0.5)
 	attack.start()
-	
+
+
 func _on_defuse_timeout() -> void:
-	position.y = randf_range(260,1000)
-	boddy.color = Color(0.797, 0.0 , 0.205 ,0.5)
+	position.y = randf_range(260, 1000)
+	boddy.color = Color(0.797, 0.0, 0.205, 0.5)
 	active = false
 	attack.start()
-	
+
+
 func _on_attack_timeout() -> void:
 	boddy.color = Color(0.0, 0.0, 0.0, 1.0)
 	active = true
-	defuse.start()
 	
+	for body in get_overlapping_bodies():
+		if body.is_in_group("gracz"):
+			Global.lifes -= 1
+	
+	defuse.start()
+
+
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("gracz") and active == true:
-		Global.lifes -=1
+		Global.lifes -= 1
--- scripts/level_up.gd
+++ scripts/level_up.gd
@@ -5,7 +5,7 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("gracz"):
-		#print("lvl_nigga")
+		#print("lvl_nigga") to chyba nie był wiatr
 		#Global.current_level += 1
 		#if Global.current_level == 1:
 			#get_tree().change_scene_to_file("res://scenes/levels/level_1.tscn")
