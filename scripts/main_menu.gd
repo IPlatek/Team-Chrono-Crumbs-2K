32b9aa710da32bbf861881a84ce61ffd50aecb26
Zegatlord <alex.krzak@icloud.com>
2026-09-17 21:42:22 +0200

    creditsy

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -33,7 +33,7 @@ func _on_options_pressed() -> void:
 
 
 func _on_credits_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/Menu/credits.tscn")
 
 
 func _on_quit_pressed() -> void:
